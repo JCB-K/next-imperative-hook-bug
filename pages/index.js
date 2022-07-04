43d0218b0d523964dffb0d56a659ab156bfeff39
Jacob Klapwijk <jacob.klapwijk@mentimeter.com>
2022-07-04 13:32:45 +0200

    repro case

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import * as React from "react";
+import NextLink from "next/link";
+
+const Link = React.forwardRef((props, ref) => {
+  React.useImperativeHandle(ref, () => ({}));
+
+  return <a {...props} />;
+});
+
+const WrappedLinkComponent = React.forwardRef(({ href, children }, ref) => {
+  return (
+    <NextLink href={href}>
+      <Link ref={ref} href={href}>
+        {children}
+      </Link>
+    </NextLink>
+  );
+});
+
 export default function Home() {
-  return null;
+  return <WrappedLinkComponent href="Somewhere">Click</WrappedLinkComponent>;
 }
